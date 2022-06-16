b2f55d72ac92e36d77cd1dae49a4b7cc9074a62e
Tibax <u1974750@campus.udg.edu>
2022-06-16 14:53:22 +0200

    9

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,12 +1,15 @@
-extends Area2D
+extends KinematicBody2D
 
 export var dir = false #false up i down; true left and right 
 export var back = false
 export var blocks = 4
+export var i = 0
+
+var animDown = false
+var fromHit = false
 
 signal playerTurn
 
-var i = 0
 
 func _ready():
 	$AnimatedSprite.flip_h = back
@@ -14,32 +17,48 @@ func _ready():
 func _mov():
 	if dir:
 		if back:
-			position.x += 16
-			position.y += 8
+			position.x += 32
+			position.y += 16
 		else:
-			position.x -= 16
-			position.y -= 8
+			position.x -= 32
+			position.y -= 16
 	else:
 		if back:
-			position.x += 16
-			position.y -= 8
+			position.x += 32
+			position.y -= 16
 		else:
-			position.x -= 16
-			position.y += 8
+			position.x -= 32 #16
+			position.y += 16 #8
 	i += 1
-	if i == blocks: 
+	if i == blocks or fromHit: 
 		back = not back
 		$AnimatedSprite.flip_h = back
 		i = 0
+	animDown = false
+	$AnimatedSprite.play("jump_down")
 	emit_signal("playerTurn")
 	
 
 func _on_Timer_timeout():
-	_mov()
+	$AnimatedSprite.play("jump_up")
 	$Timer.wait_time = 1
-
-
-
 
 func _on_Rat_enemyTurn():
 	$Timer.start()
+
+
+func _on_AnimatedSprite_animation_finished():
+	if $AnimatedSprite.animation == "jump_up":
+		_mov()
+	elif $AnimatedSprite.animation == "jump_down":
+		animDown = true
+		$AnimatedSprite.play("idle")
+
+
+func _on_Area2D_area_entered(area):
+	print("collision")
+	if area.name == "AreaRat":
+		print("COLLISION")
+		back = not back
+		fromHit = true
+		_mov()
